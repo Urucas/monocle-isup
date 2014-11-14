5587999471dddbf09f691577505996f908dc88dd
Bruno Alassia <vruno.alassia@gmail.com>
2014-11-13 23:36:22 -0300

    add example

---
+++ example/app.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+'use strict'
+var monocle = require('monocle-js'),
+	isUp = require("monocle-isup"),
+
+monocle.run(function*(){
+	var data = yield isUp("urucas.com");
+	if(data) {
+		console.log("is up");
+	}else {
+		console.log("is down");
+	}
+});
+
